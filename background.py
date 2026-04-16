81a8893bcc5cf9dadff14823ce617437076d75a5
DU4KA <lazyrnoe1more@gmail.com>
2026-04-16 18:29:22 +0300

    added moving background image

---
+++ background.py
@@ -0,0 +1,20 @@
+import pygame
+from config import *
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, layer=0, speedy=5, y=0):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(pygame.image.load("images/bg.png").convert(), (WIDTH, HEIGHT + 5))
+        self.rect = self.image.get_rect()
+        self.layer = layer
+        self.speedy = speedy
+        self.rect.y = y
+
+    def update(self):
+        self.rect.y += self.speedy
+
+    def is_in_centre(self):
+        if HEIGHT + 5 <= self.rect.bottom and self.rect.bottom <= HEIGHT + 5:
+            return True
+        return False
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import math
-import random
-from random import choice
-
 from player import Player
 from car_bot import *
 from white_line import *
+from background import *
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -12,8 +10,15 @@ pygame.display.set_caption("race")
 
 all_sprites = pygame.sprite.LayeredUpdates()
 cars = pygame.sprite.Group()
-car_player = Player(RED, (CAR_W, CAR_H), layer=2)
+
+background_speed = 5
+bg1 = Background(y=5, speedy=background_speed)
+bg2 = Background(y=-HEIGHT, speedy=background_speed)
+car_player = Player(RED, (CAR_W, CAR_H), layer=3)
+
 all_sprites.add(car_player)
+all_sprites.add(bg1, layer=0)
+all_sprites.add(bg2, layer=0)
 
 
 clock = pygame.time.Clock()
@@ -29,19 +34,25 @@ while running:
 
     # Calculations
     if math.fabs(pygame.time.get_ticks() - car_last_spawn_time) >= car_spawn_times:
-        cars.add(CarBot(random.choice([RED, GREEN]), random.randint(4, 7), layer=1))
-        cars.add(CarBot(random.choice([RED, GREEN]), random.randint(4, 7), layer=1))
-        cars.add(CarBot(random.choice([RED, GREEN]), random.randint(4, 7), layer=1))
-        cars.add(CarBot(random.choice([RED, GREEN]), random.randint(4, 7), layer=1))
+        cars.add(CarBot(random.choice([YELLOW, GREEN]), random.randint(4, 7), layer=2))
         car_last_spawn_time = pygame.time.get_ticks()
         car_spawn_times = random.choice([1500, 2000, 1750, 1000])
 
     if math.fabs(pygame.time.get_ticks() - line_last_spawn_time) >= 500:
         line_last_spawn_time = pygame.time.get_ticks()
-        all_sprites.add(WhiteLinePart(WHITE, 5, layer=0))
+        all_sprites.add(WhiteLinePart(WHITE, speedy=background_speed, layer=1))
+
 
     all_sprites.update()
     cars.update()
+
+    if bg1.is_in_centre():
+        bg2.rect.y = -HEIGHT
+        print("background 1 moved")
+
+    if bg2.is_in_centre():
+        bg1.rect.y = -HEIGHT
+        print("background 2 moved")
 
     for car in cars:
         if car.is_on_the_edge():
